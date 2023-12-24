c0489f7a7d73ade569ffb86e510430e3538387bb
n0bisuke <rsksound@gmail.com>
2023-12-24 20:04:51 +0900

    init

---
+++ hwLimitNotify.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const getHWInfo = require('./hwLimitNotify/_getHWInfo');
+const getClassInfo = require('./hwLimitNotify/_getClassInfo');
+const postDiscord = require('./hwLimitNotify/_postDiscord');
+
+const main = async () => {
+    try {
+        const kadaiItems = await getHWInfo();
+
+        // 課題の情報にクラスの情報を追加 - 主にWebhook用
+        for (let i = 0, len = kadaiItems.length; i < len; i++) {
+            const classInfo = await getClassInfo(kadaiItems[i].classId);
+            kadaiItems[i].classInfo = classInfo;
+        }
+        //console.log(kadaiItems);
+
+        // Discordに送信
+        for (let i = 0, len = kadaiItems.length; i < len; i++) {
+            const res = await postDiscord(kadaiItems[i]); //Discordにポスト
+            console.log(res.status);
+        }
+
+    } catch (error) {
+      console.error(error)
+    }
+}
+
+main();
