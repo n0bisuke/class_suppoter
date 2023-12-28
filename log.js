73d95d3258d99cebcab1c4d5e0c2f76acc40abbd
n0bisuke <rsksound@gmail.com>
2023-12-28 21:30:39 +0900

    deebug

--- log.js
+++ log.js
@@ -11,11 +11,16 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Tokyo");
 
+console.log(`--TESTENV---`)
+console.log(process.env.TESTENV1, JSON.parse(process.env.TESTENV1));
+console.log(process.env.TESTENV2, JSON.parse(process.env.TESTENV2));
+console.log(process.env.TESTENV3, JSON.parse(process.env.TESTENV3));
+console.log(process.env.TESTENV4, JSON.parse(process.env.TESTENV4));
+console.log(process.env.TESTENV5, JSON.parse(process.env.TESTENV5));
+console.log(process.env.TESTENV6, JSON.parse(process.env.TESTENV6));
 
 (async () => {
     try {
-        const hoge = process.env.GOOGLE_CLIENT_SECRET;
-        console.log(hoge, typeof hoge);
         //ロギング
         const logjson = {
             msg: 'success',
